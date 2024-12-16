9abdac04b7e0dcb64344962c70ec4cc8f4df1c28
polpip <104210825+polpip@users.noreply.github.com>
2024-12-16 18:41:53 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,64 @@
+class House:
+    def __init__(self, name, number_of_floors):
+     self.name = name
+     self.number_of_floors = number_of_floors
+
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+    def __str__(self):
+     return (f"название:{self.name},колличество этажей {self.number_of_floors}")
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+    def __add__(self, floors):
+        if isinstance(floors, int):
+            return House(self.name, self.number_of_floors + floors)
+        return NotImplemented
+    def __iadd__(self, floors):
+            return self.__add__(floors)
+    def __radd__(self, floors):
+        return self.__add__(floors)
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+    def go_to(self, new_floor):
+        new_floor = int(new_floor)
+        for i in range(1, new_floor + 1):
+            print(i)
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
